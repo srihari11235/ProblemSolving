ad48d7aa6daf93f888af2993ebb8fa8dbf0e7c75
Srihari Haridasan <Srihari.Haridasan@Neudesic.com>
2020-08-04 20:36:24 +0530

    Day 19 problem

---
+++ TechGig/30 day code challenge/Day 19 - Binary to Decimal/Day 19 - Binary to Decimal.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class CandidateCode {
+    static void Main(String[] args) {
+                           
+            int binaryNumber = int.Parse(Console.ReadLine());
+            int decimalValue = 0;
+            int base1 = 1;
+            
+            while (binaryNumber > 0)
+            {
+                int reminder = binaryNumber % 10;
+                binaryNumber = binaryNumber / 10;
+                decimalValue += reminder * base1;
+                base1 = base1 * 2;
+            }
+            Console.Write(decimalValue);
+    }
+}
---
+++ TechGig/30 day code challenge/Day 19 - Binary to Decimal/README.md
@@ -0,0 +1,24 @@
+
+# Binary To Decimal (100 Marks)<br/>
+You will be getting a binary number as an input and you need to convert it into a decimal number.<br/>
+<br/>
+### Input Format<br/>
+You will be taking a binary number as an input. <br/>
+<br/>
+### Constraints<br/>
+1 < N < 10^9<br/>
+<br/>
+### Output Format<br/>
+Print the decimal number to the stdout. <br/>
+<br/>
+## Sample TestCase 1<br/>
+<br/>
+### Input <br/>
+110<br/>
+<br/>
+### Output<br/>
+6<br/>
+<br/>
+### Explanation<br/>
+<br/>
+This is a traditional conversion which needs to be implemented in programming.<br/>
