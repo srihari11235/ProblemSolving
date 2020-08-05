4d2718e1a23ab774af83c317503f22d144140de7
Srihari Haridasan <Srihari.Haridasan@Neudesic.com>
2020-08-05 21:46:33 +0530

    Day 22 - problem

---
+++ TechGig/30 day code challenge/Day 22 - War between odd and even/Day 22 - War between odd and even.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class CandidateCode {
+    static void Main(String[] args) {
+	   
+        var code = new CandidateCode();
+        
+        var numberOfElements = code.ReadNumberOfElments();
+        var numbers = code.ReadNumbers();
+        var sumOfOddNumbers = 0;
+        var sumOfEvenNumbers = 0;
+
+       for(int i = 0; i < numbers.Count; i++)
+       {
+            if(i == 0)                            
+                sumOfEvenNumbers += numbers[i];                           
+            else if(i == 1)                            
+                sumOfOddNumbers += numbers[i];                                      
+            else if(code.IsIndexEven(i))                            
+                sumOfEvenNumbers += numbers[i];                            
+            else                            
+                sumOfOddNumbers += numbers[i];                            
+       }
+
+       if(sumOfEvenNumbers < sumOfOddNumbers)
+        Console.Write(sumOfOddNumbers - sumOfEvenNumbers);
+       else
+         Console.Write(sumOfEvenNumbers - sumOfOddNumbers);
+
+    }
+
+    private int ReadNumberOfElments()
+    {
+        var input = Console.ReadLine();
+
+        return Int32.Parse(input);
+    }
+
+    private List<int> ReadNumbers()
+    {
+        var input = Console.ReadLine();
+        var characters = input.Split(' ');
+        var numbers = new List<int>();
+
+        foreach(var charachter in characters)
+        {
+            numbers.Add(Int32.Parse(charachter));
+        }
+
+        return numbers;
+    }
+
+    private bool IsIndexEven(int number)
+    {
+        return number % 2 == 0;
+    }
+}
