8ac5edd2331d1888003360aa1cf8b6e9ced2d385
srihari11235 <srihari11235@gmail.com>
2021-01-18 09:13:47 +0530

    Qeueu basic

---
+++ Queue/Queue - Basic/Queue - Basic/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Queue___Basic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var run = new Program();
+
+            var result = run.ReverseStringForTheGivenNumberOfTimes(5, "Learning Queue Functions");
+
+            //Print the reversed string
+            foreach (char letter in result)
+            {
+                Console.Write(letter);
+            }
+        }
+
+        private Queue<char> ReverseStringForTheGivenNumberOfTimes(int n, string word)
+        {
+            var letterQueue = new Queue<char>();
+            
+            foreach(var letter in word)
+            {
+                letterQueue.Enqueue(letter);
+            }
+
+            for(var i =0; i<n; i++)
+            {
+                if (letterQueue.Count == 0)
+                    break;
+
+                var ch = letterQueue.Peek();
+
+                letterQueue.Dequeue();
+                letterQueue.Enqueue(ch);
+            }
+
+            return letterQueue;
+        }
+    }
+}
